5214504fe01ee4e5532fcbbce31d358ec6867f38
jakub.rudnik <jakub.rudnik@siecbudowlanaexport.pl>
2024-04-29 09:34:42 +0200

    Fix typography and line-height

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,10 +2,11 @@ import type {ReactNode} from "react";
 
 export interface ITitleProps {
     children: ReactNode;
+    className: string;
 }
 
-export default function Title({children}: ITitleProps) {
+export default function Title({children, className}: ITitleProps) {
     return (
-        <h2 className="text-2xl md:text-[40px] font-bold">{children}</h2>
+        <h2 className={"text-2xl sm:text-4xl font-bold lg:leading-[3rem] " + className}>{children}</h2>
     )
 }
