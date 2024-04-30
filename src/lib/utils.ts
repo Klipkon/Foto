dcad129a41d5d6dc7b769395070f97be45202758
jakub.rudnik <jakub.rudnik@siecbudowlanaexport.pl>
2024-04-30 14:46:55 +0200

    createsd func to check mobile devices and added it to turn off animation if device is mobile

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isMobile() {
+  const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return regex.test(navigator.userAgent);
+}
