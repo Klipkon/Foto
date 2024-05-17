4762de526271eb2536b020804c61e28d317a5fbb
jakub.rudnik <jakub.rudnik@siecbudowlanaexport.pl>
2024-05-17 07:29:48 +0200

    Add article page and container with articles cards

---
+++ src/interfaces/article.interface.ts
@@ -0,0 +1,77 @@
+export interface Aricle {
+    data: ArticleData[];
+    meta: Meta;
+}
+
+export interface ArticleData {
+    id:         number;
+    attributes: DatumAttributes;
+}
+
+export interface DatumAttributes {
+    title:       string;
+    categorie:   string;
+    content:     string;
+    createdAt:   Date;
+    updatedAt:   Date;
+    publishedAt: Date;
+    slug:        null;
+    image:       Image;
+}
+
+export interface Image {
+    data: Data;
+}
+
+export interface Data {
+    id:         number;
+    attributes: DataAttributes;
+}
+
+export interface DataAttributes {
+    name:              string;
+    alternativeText:   null;
+    caption:           null;
+    width:             number;
+    height:            number;
+    formats:           Formats;
+    hash:              string;
+    ext:               string;
+    mime:              string;
+    size:              number;
+    url:               string;
+    previewUrl:        null;
+    provider:          string;
+    provider_metadata: null;
+    createdAt:         Date;
+    updatedAt:         Date;
+}
+
+export interface Formats {
+    thumbnail: Small;
+    small:     Small;
+}
+
+export interface Small {
+    name:        string;
+    hash:        string;
+    ext:         string;
+    mime:        string;
+    path:        null;
+    width:       number;
+    height:      number;
+    size:        number;
+    sizeInBytes: number;
+    url:         string;
+}
+
+export interface Meta {
+    pagination: Pagination;
+}
+
+export interface Pagination {
+    page:      number;
+    pageSize:  number;
+    pageCount: number;
+    total:     number;
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,7 @@ export function isMobile() {
   const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
   return regex.test(navigator.userAgent);
 }
+
+export function titleToSlug(title: string) {
+  return title.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase().replace(/ /g, "-").replace(/[^a-z0-9-]/g, "")
+}
