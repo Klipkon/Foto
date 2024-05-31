9f5c790c2dd1f97a482d6121c285a4678a8ac6f0
jakub.rudnik <jakub.rudnik@siecbudowlanaexport.pl>
2024-05-31 13:12:17 +0200

    Pagination, categories routes and fixed breadcrumbs

--- src/components/ArticlesCards.tsx
+++ src/components/ArticlesCards.tsx
@@ -4,34 +4,37 @@ import { Card, CardContent, CardTitle } from "./ui/card";
 import { filterArticlesByCategory, titleToSlug } from "@/lib/utils";
 
 export default function ArticlesCards({ articles, categories, portfolio}: { articles: ArticleData[], categories: CategoryData[], portfolio?: boolean}) {
-    console.log(articles, categories);
+   
+    function currentCategory() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const category = urlParams.get('category');
+
+        return category ? category : "Wszystko";
+    }
+
+    function setCategoryInUrl(category: string) {
+        const urlParams = new URLSearchParams(window.location.search);
+
+        urlParams.set('category', category);
+
+        window.location.search = urlParams.toString();
+    }
+
+    
 
     return (
-        <Tabs defaultValue="Wszystko">
+        <Tabs defaultValue={currentCategory()}>
             <TabsList>
             {
                 categories.map((category, i) => (
-                    <TabsTrigger key={i} value={category.attributes.name}>{category.attributes.name}</TabsTrigger>
+                    <TabsTrigger key={i} value={category.attributes.name} onMouseDown={() => setCategoryInUrl(category.attributes.name)}>{category.attributes.name}</TabsTrigger>
                 ))
             }
             </TabsList>
             {
                 categories.map((category, i) => (
                     <TabsContent key={i} value={category.attributes.name}>
-                        <div id="cardsContainer" className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 xl:gap-10 pt-14">
-                        {
-                            filterArticlesByCategory(articles, category.attributes.name).map((article, i) => (
-                                <a key={article.id} href={(portfolio ? "/portfolio/" : "/blog/") + titleToSlug(article.attributes.title)} className="group">
-                                    <Card className="p-6 lg:p-8 border-none shadow-none bg-secondary h-full">
-                                        <CardContent className="flex flex-col justify-beetwen items-start p-0 gap-5 md:gap-6">
-                                            <img className="w-full rounded-lg max-h-[302px]" src={import.meta.env.PUBLIC_STRAPI_URL + article.attributes.image.data.attributes.formats.thumbnail.url} alt="" width={article.attributes.image.data.attributes.formats.thumbnail.width} height={article.attributes.image.data.attributes.formats.thumbnail.height} loading="eager" />
-                                            <CardTitle className="font-semibold text-lg sm:text-2xl">{article.attributes.title}</CardTitle>
-                                        </CardContent>
-                                    </Card>
-                                </a>
-                            ))
-                        }
-                        </div>
+                       
                     </TabsContent>
                 ))
             }
