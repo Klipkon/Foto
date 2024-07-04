19b5d2f5802490f0f11bafc76fd045167efdc324
JakubKlipkon <jakub.rudnik@siecbudowlanaexport.pl>
2024-07-04 15:43:44 +0200

    Refactor: Change button styles and behaviors to suit to the new design

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,56 +1,72 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
+const buttonVariants = cva("", {
+  variants: {
+    variant: {
+      default:
+        "bg-primary text-primary-foreground border-1 border-black hover:border-white",
+      secondary: "bg-white text-black border-[1px] border-black",
+      ghost:
+        "bg-background border-1 border-white hover:bg-black hover:text-white hover:shadow-[3px_3px_0_0_rgba(0,0,0,1)]",
     },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
+    size: {
+      default: "h-10 px-4 py-2",
+      sm: "h-9 rounded-md px-3",
+      lg: "h-11 rounded-md px-8",
+      icon: "h-10 w-10",
     },
-  }
-)
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          size,
+          className,
+          "w-fit text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative group"
+        )}
         ref={ref}
         {...props}
-      />
-    )
+      >
+        <div
+          className={cn(
+            buttonVariants({ variant, size }),
+            variant == "ghost"
+              ? ""
+              : "group-hover:-translate-x-[3px] group-hover:-translate-y-[3px]",
+            "rounded-md inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative transition-transform"
+          )}
+        >
+          {props.children}
+        </div>
+        <div
+          className={cn(
+            variant == "default" ? "bg-accent" : "bg-black",
+            variant == "ghost" ? "hidden" : "block",
+            "w-full h-full absolute top-0 left-0 z-[-1] rounded-md"
+          )}
+        ></div>
+      </Comp>
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
