8131d2c34f202a00f200ecba2264d2f70039b7cb
JakubKlipkon <jakub.rudnik@siecbudowlanaexport.pl>
2024-07-17 11:19:34 +0200

    add new env var type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly STRAPI_URL: string;
-    readonly PUBLIC_STRAPI_URL: string;
-    readonly STRAPI_API_TOKEN: string;
+  readonly STRAPI_URL: string;
+  readonly PUBLIC_STRAPI_URL: string;
+  readonly STRAPI_API_TOKEN: string;
+  readonly PUBLIC_GOOGLE_RECAPTCHA_SECRET: string;
 }
